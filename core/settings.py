8c656f57952c8d77bec0812e46c6dc39ef82f603
David <oscardavidestradabetancourth@gmail.com>
2025-04-05 08:31:49 -0500

    fex(static) = add link for staticfiles

--- core/settings.py
+++ core/settings.py
@@ -125,7 +125,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'app/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
