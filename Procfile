2b151c16d28bb20ad6f2e798ea7ef884d0212bbc
David <oscardavidestradabetancourth@gmail.com>
2025-04-05 08:11:59 -0500

    fex(desploy) = staticfiles

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web : gunicorn core.wsgi
+
+web : python manage.py collectstatic && unicorn core.wsgi
--- core/settings.py
+++ core/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'app',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,4 +132,11 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
